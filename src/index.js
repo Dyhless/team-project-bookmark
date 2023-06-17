7892eda47555f6955660ca660133573c6a561983
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-17 19:58:40 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './js/book-card';
-import './js/books-service';
-import './js/dark-theme';
+import './js/book-card.js';
+import './js/books-service.js';
+import './js/dark-theme.js';
 import './js/fonds-info.js';
 import './js/modal-window.js';
-import './js/slider';
+import './js/slider.js';
