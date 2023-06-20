d62c5bc433f3f922fed1f389f210213927837675
Oksana-perederii <novamova.nemchenko@gmail.com>
2023-06-20 19:55:14 +0300

    header

--- src/js/header-fixed-position.js
+++ src/js/header-fixed-position.js
@@ -10,17 +10,3 @@ window.addEventListener('scroll', function () {
     header.classList.remove('fixed-header');
   }
 });
-
-const refs = {
-  currentPage: window.location.href,
-  navItems: document.querySelectorAll('.site-nav__link'),
-};
-
-refs.navItems.forEach(addUnderLine);
-
-function addUnderLine(navItem) {
-  if (navItem.href === refs.currentPage) {
-    refs.navItems[0].classList.remove('site-nav__link--current');
-    navItem.classList.add('site-nav__link--current');
-  }
-}
