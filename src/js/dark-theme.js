48a1ab99a3ba36ea32a4aad3618e252f0ed963b7
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-20 12:33:16 +0200

    Revert "Merge pull request #57 from Dyhless/dark-and-header"
    This reverts commit 1cdd9d03c5b70a0e9359886dd72435671c47b384, reversing
    changes made to 70b22a826e0b9acdb36f67e8cb423cfa5f8fe77f.

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,53 +1,61 @@
 const html = document.querySelector('html');
 const themeToggle = document.querySelector('#switch');
 
+
 themeToggle.addEventListener('change', onToggleTheme);
 
-// 1.При першому завантаженні сторінки перевіряє, яка тема збережена в ЛС, якщо темна, то ставить перемикач теми в
+
+// 1.При першому завантаженні сторінки перевіряє, яка тема збережена в ЛС, якщо темна, то ставить перемикач теми в 
 // відповідне положення
 window.addEventListener('DOMContentLoaded', () => {
-  // Отримуємо збережену тему з LS
-  const savedTheme = localStorage.getItem('theme');
-  
-  if (savedTheme === 'dark') {
+  if (localStorage.getItem('theme') === 'dark') {
     themeToggle.checked = true;
-    applyTheme('dark');
-  } else {
-    applyTheme('light');
   }
-  
-  // Сінхронізуємо тему, встановлену у користувача в ОС, з темою сайту, якщо нічого не записано в LS
+  // сінхронізує тему, встановлену у користувача в ОС, з темою сайту, якщо в ЛС нічого не записано
   syncThemeWithOS();
 });
 
 // Функція, яка викликається при кліку на світчер, викликає функцію додавання/видалення атрибуту темної теми
 // Якщо в ЛС записана темна тема, то її видаляємо при кліку на світчер і встановиться тема за змовченням - світла
+// Якщо нічого немає в ЛС, тоді записуємо тему темну 
 function onToggleTheme(evt) {
-  if (themeToggle.checked) {
-    applyTheme('dark');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    applyTheme('light');
+  // evt.preventDefault();
+  addDarkAttribute();
+
+  if (localStorage.getItem('theme') === 'dark') {
     localStorage.removeItem('theme');
-  }
-}
-
-// Функція для встановлення теми
-function applyTheme(theme) {
-  if (theme === 'dark') {
-    html.setAttribute('data-theme', 'dark');
   } else {
-    html.removeAttribute('data-theme');
+    localStorage.setItem('theme', 'dark');
   }
 }
 
-// Функція синхронізації теми з ОС користувача з темою сайту
+// Додає атрибут темної теми до html
+// Якщо атрибут вже є, його видяляємо, якщо немає - додаємо
+function addDarkAttribute() {
+  if (html.hasAttribute('data-theme')) {
+    html.removeAttribute('data-theme');
+  } else {
+    html.setAttribute('data-theme', 'dark');
+  }
+}
+
+// Функція сінхронізації теми сайту з ЛС після перезавантаження 
+function syncThemeWithLocalStorage() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    addDarkAttribute();
+    themeToggle.checked = true;
+  }
+}
+
+// Функція синхронізації теми з ОС користувача з темой сайту
 function syncThemeWithOS() {
   const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  
   if (prefersDarkMode && !themeToggle.checked) {
     onToggleTheme();
   } else if (!prefersDarkMode && themeToggle.checked) {
     onToggleTheme();
   }
 }
+
+syncThemeWithLocalStorage();
