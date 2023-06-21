67798f93419c47b9d366656ecc32ff5590656b67
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-21 20:48:54 +0200

    update

--- src/js/books-service.js
+++ src/js/books-service.js
@@ -2,18 +2,8 @@ import axios from 'axios';
 
 export class BooksService {
   #BASE_URL = 'https://books-backend.p.goit.global/books/';
-  bookID = null;
 
   async fetchTopBooks() {
     return await axios.get(`${this.#BASE_URL}/top-books`).catch(console.error);
-  }
-
-  async fetchBookById(id) {
-    try {
-      const response = await axios.get(`${this.#BASE_URL}/${id}`);
-      return response.data;
-    } catch (error) {
-      console.log(error.message);
-    }
   }
 }
