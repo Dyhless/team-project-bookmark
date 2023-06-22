998fdd6a80483770ab03582fe8fa92efe570bb73
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-06-22 15:35:53 +0300

    correct colors

--- src/js/current-page.js
+++ src/js/current-page.js
@@ -1,10 +1,12 @@
 const pageShoppingList = document.querySelector('.shopping-list');
 const pageHome = document.querySelector('.home');
+// const currentShopList = document.querySelector('.shopping-list.site-nav__link--current');
 
 // Додати клас до поточного посилання при переході на сторінку
 window.addEventListener('DOMContentLoaded', () => {
   if (window.location.pathname.endsWith('shopping-list.html')) {
     pageShoppingList.classList.add('site-nav__link--current');
+    
     pageHome.classList.remove('site-nav__link--current');
   } else if (window.location.pathname.endsWith('index.html')) {
     pageHome.classList.add('site-nav__link--current');
